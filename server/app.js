3d82159fc69d6898b920aa463493e7c584ded220
Apu Chandra Das <apuchandradas5568@gmail.com>
2024-04-03 15:35:21 +0530

    done with auth routes , register , login and logout

--- server/app.js
+++ server/app.js
@@ -1,10 +1,14 @@
 import express from "express"
+import postRoutes from "./routes/post_routes.js"
+import userRoutes from "./routes/user_routes.js";
+import authRoutes from "./routes/auth_routes.js"
 
 const app = express()
+app.use(express.json())
 
-app.use('/api/test', (req,res) =>{
-    res.send("Welcome to the test API")
-})
+app.use('/api/users',  userRoutes)
+app.use('/api/posts', postRoutes)
+app.use('/api/auth', authRoutes)
 
 
 app.listen(8080, ()=>{
---
+++ server/controllers/auth_controller.js
@@ -0,0 +1,71 @@
+import bcrypt from "bcrypt";
+import prisma from "../lib/prisma.js";
+import jwt from "jsonwebtoken";
+
+export const register = async (req, res) => {
+  const { username, email, password } = req.body;
+
+  try {
+    // hash the password
+    const hashedPassword = await bcrypt.hash(password, 10);
+    // create a new user and save to do
+
+    const newUser = await prisma.user.create({
+      data: {
+        username,
+        email,
+        password: hashedPassword,
+      },
+    });
+
+    res.status(201).json({ message: "User Created Successcully" });
+  } catch (error) {
+    res.status(500).json({ message: "Failed to Create User" });
+  }
+};
+export const login = async (req, res) => {
+
+  const { email, password } = req.body;
+  try {
+    // checking the user is exist or not
+    const user = await prisma.user.findUnique({
+      where: {
+        email,
+      },
+    });
+
+    if (!user) return res.status(400).json({ message: "Invalid Credentials" });
+
+    //    checking the passwor is correct or not
+    const isPasswordValid = await bcrypt.compare(password, user.password);
+
+    if (!isPasswordValid)
+      return res.status(400).json({ message: "Invalid credentials" });
+
+    // generating token
+    const token = jwt.sign(
+      { userId: user.id, isAdmin: false },
+      process.env.JWT_SECRET,
+      { expiresIn: "30d" }
+    );
+
+    // seperating password
+    const { password: userPassword, ...userInfo } = user;
+
+    res
+      .cookie("user", token, {
+        httpOnly: true,
+        //   maxAge: "30d",
+      })
+      .status(200)
+      .json({
+        userInfo,
+      });
+  } catch (error) {
+    res.status(500).json({ message: "Failed to Login" });
+
+  }
+};
+export const logout = async (req, res) => {
+  res.clearCookie("user").status(200).json({ message: "Logout Successfull" });
+};
